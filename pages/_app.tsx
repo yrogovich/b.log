a9adbfdd30c08ba73e63463df6b62d489a60975a
Yaroslav Rogovich <yrogovich@gmail.com>
2022-08-20 15:07:45 +0200

    [npm] add fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/index.css'
+
+config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
