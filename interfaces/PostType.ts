e28cede6b1335ef6a99f9962c71db70c56c1ec6c
Yaroslav Rogovich <yrogovich@gmail.com>
2022-10-30 18:53:06 +0100

    [global] add icon to types

--- interfaces/PostType.ts
+++ interfaces/PostType.ts
@@ -1,10 +1,11 @@
 type PostType = {
-    slug: string
-    title: string
-    date: string
-    coverImage: string
-    excerpt: string
-    content: string
+  slug: string
+  icon: string
+  title: string
+  date: string
+  coverImage: string
+  excerpt: string
+  content: string
 }
 
 export default PostType
