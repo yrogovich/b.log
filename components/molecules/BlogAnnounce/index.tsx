3c24917a5113102c312a7a7f69632ff80daa2daf
Yaroslav Rogovich <yrogovich@gmail.com>
2023-04-10 17:49:32 +0200

    feat: animation experiment

--- components/molecules/BlogAnnounce/index.tsx
+++ components/molecules/BlogAnnounce/index.tsx
@@ -1,67 +1,24 @@
 import PostType from 'interfaces/PostType'
 import Link from 'next/link'
-import {motion} from 'framer-motion'
 import styles from './BlogAnnounce.module.scss'
 
 const BlogAnnounce = ({post}) => {
   const {title, icon, slug, excerpt}: PostType = post
 
-  const itemAnimation = {
-    hidden: {
-      opacity: 0,
-      y: 100,
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: .5,
-      },
-    },
-  }
-
-  const linkAnimation = {
-    hidden: {
-      opacity: 0,
-      y: 100,
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: .5,
-        staggerChildren: .2,
-      },
-    },
-    exit: {
-      opacity: 0,
-      y: -100,
-      transition: {
-        duration: .5,
-        staggerChildren: .2,
-      },
-    },
-  }
-
   return (
-    <motion.div
+    <div
       className={styles.announce}
-      variants={itemAnimation}
     >
       <Link as={`/posts/${slug}`} href="/posts/[slug]">
-        <motion.h2
+        <h2
           className={styles.title}
-          variants={linkAnimation}
         >
           {icon} <span className={styles.link}>{title}</span>
-        </motion.h2>
+        </h2>
       </Link>
 
-      <motion.p
-        className={styles.text}
-        variants={linkAnimation}
-      >{excerpt}</motion.p>
-    </motion.div>
+      <p className={styles.text}>{excerpt}</p>
+    </div>
   )
 }
 
